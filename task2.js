37b78b58fda9f292e7c1e694251733887d37f859
maisa abusalem <heart.webd@gmail.com>
2021-12-24 11:49:38 +0100

    change file name to task2

---
+++ task2.js
@@ -0,0 +1,22 @@
+/*In this task you will work with numbers / integers and strings.
+The computer can read numeric values as they are but not characters.
+Use quotation marks to enter characters as data.
+*/
+
+
+var name = "Lisa" //Your name as a string;
+var age = 40 //your age as a number;
+var height = 156 //Your length as a number in cm;
+var weight = 62 //Your weight as a number in kg;
+var eyes = "brown"//Color of your eyes as a string;
+var hair = "brown"//Color of your hair as a string;
+var sum = age + height + weight;
+
+console.log("Here is " + name + ".");
+console.log(name + " is " + age + " years old");
+console.log(name + " is " + height + "cm.");
+console.log(name + " weight " + weight + "kg.");
+console.log(name + " has " + eyes + " eyes and " + hair + " hair");
+
+
+console.log("If we add up " + name + "'s age weight and height we get " + sum);
